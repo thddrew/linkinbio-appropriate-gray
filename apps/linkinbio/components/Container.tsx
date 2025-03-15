2cec3cb99b023ce306c6736f00d1df45513cc5db
espressotonicstudio <espressotonicstudio@gmail.com>
2025-03-15 12:42:41 -0700

    fix: container height

--- apps/linkinbio/components/Container.tsx
+++ apps/linkinbio/components/Container.tsx
@@ -15,12 +15,12 @@ export const Container = async ({
   return (
     <div
       className={cn(
-        "relative min-h-full h-full px-4 pb-8 pt-10",
+        "relative min-h-full px-4 pb-8 pt-10",
         theme?.font.body.className,
         theme?.container?.className
       )}
     >
-      <main className="max-w-md h-full mx-auto flex flex-col gap-8 relative z-10">
+      <main className="max-w-md min-h-full mx-auto flex flex-col gap-8 relative z-10">
         {children}
       </main>
       {backgroundImage && (
